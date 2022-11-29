88be4de804da3637796ddafb926a30cbd7bbf092
Jannik Wagner <jannik.wagner@proton.me>
2022-11-29 16:06:29 +0100

    init dqn - wip

--- lab2/problem1/DQN_problem.py
+++ lab2/problem1/DQN_problem.py
@@ -20,6 +20,9 @@ import torch
 import matplotlib.pyplot as plt
 from tqdm import trange
 from DQN_agent import RandomAgent
+import random
+import queue
+
 
 def running_average(x, N):
     ''' Function used to compute the running average
@@ -32,6 +35,7 @@ def running_average(x, N):
         y = np.zeros_like(x)
     return y
 
+
 # Import and initialize the discrete Lunar Laner Environment
 env = gym.make('LunarLander-v2')
 env.reset()
@@ -40,6 +44,9 @@ env.reset()
 N_episodes = 100                             # Number of episodes
 discount_factor = 0.95                       # Value of the discount factor
 n_ep_running_average = 50                    # Running average of 50 episodes
+# Number of episodes between each update of the target network
+C = 100
+buffer_size = 10000                          # replay buffer size
 n_actions = env.action_space.n               # Number of available actions
 dim_state = len(env.observation_space.high)  # State dimensionality
 
@@ -51,54 +58,74 @@ episode_number_of_steps = []   # this list contains the number of steps per epis
 # Random agent initialization
 agent = RandomAgent(n_actions)
 
-### Training process
+# Training process
 
 # trange is an alternative to range in python, from the tqdm library
 # It shows a nice progression bar that you can update with useful information
-EPISODES = trange(N_episodes, desc='Episode: ', leave=True)
 
-for i in EPISODES:
-    # Reset enviroment data and initialize variables
-    done = False
-    state = env.reset()
-    total_episode_reward = 0.
-    t = 0
-    while not done:
-        # Take a random action
-        action = agent.forward(state)
 
-        # Get next state and reward.  The done variable
-        # will be True if you reached the goal position,
-        # False otherwise
-        next_state, reward, done, _ = env.step(action)
+def dqn(env, agent, N_episodes, C, buffer_size, discount_factor):
+    # TODO: initialize theta and phi
+
+    # initialize the replay buffer TODO: gym function?
+    # buffer = queue.Queue(maxsize=buffer_size)
+    buffer = []
+
+    EPISODES = trange(N_episodes, desc='Episode: ', leave=True)
+    for k in EPISODES:
+        # Reset enviroment data and initialize variables
+        done = False
+        s = env.reset()
+        total_episode_reward = 0.
+        t = 0
+        while not done:
+            # TODO: Take epsilon greedy action wrt Q_theta. Maybe gym?
+            a = agent.forward(s)
+
+            # Get next state and reward.
+            next_s, r, done, _ = env.step(a)
+            obs = (s, a, r, next_s, done)
+            if len(buffer) < buffer_size:
+                buffer.append(obs)
+            else:
+                # buffer.get()
+                # buffer.put(obs)
+                # buffer[random.randint(0, buffer_size-1)] = obs
+                buffer.pop(0)
+                buffer.append(obs)
+            batch = random.sample(buffer, 32)
 
         # Update episode reward
-        total_episode_reward += reward
+            total_episode_reward += r
 
         # Update state for next iteration
-        state = next_state
-        t+= 1
+            s = next_s
+            t += 1
 
     # Append episode reward and total number of steps
-    episode_reward_list.append(total_episode_reward)
-    episode_number_of_steps.append(t)
+        episode_reward_list.append(total_episode_reward)
+        episode_number_of_steps.append(t)
 
     # Close environment
-    env.close()
+        env.close()
 
     # Updates the tqdm update bar with fresh information
     # (episode number, total reward of the last episode, total number of Steps
     # of the last episode, average reward, average number of steps)
-    EPISODES.set_description(
-        "Episode {} - Reward/Steps: {:.1f}/{} - Avg. Reward/Steps: {:.1f}/{}".format(
-        i, total_episode_reward, t,
-        running_average(episode_reward_list, n_ep_running_average)[-1],
-        running_average(episode_number_of_steps, n_ep_running_average)[-1]))
+        EPISODES.set_description(
+            "Episode {} - Reward/Steps: {:.1f}/{} - Avg. Reward/Steps: {:.1f}/{}".format(
+                k, total_episode_reward, t,
+                running_average(episode_reward_list, n_ep_running_average)[-1],
+                running_average(episode_number_of_steps, n_ep_running_average)[-1]))
+
+
+dqn(env, agent, N_episodes, C, buffer_size, discount_factor)
 
 
 # Plot Rewards and steps
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(16, 9))
-ax[0].plot([i for i in range(1, N_episodes+1)], episode_reward_list, label='Episode reward')
+ax[0].plot([i for i in range(1, N_episodes+1)],
+           episode_reward_list, label='Episode reward')
 ax[0].plot([i for i in range(1, N_episodes+1)], running_average(
     episode_reward_list, n_ep_running_average), label='Avg. episode reward')
 ax[0].set_xlabel('Episodes')
@@ -107,7 +134,8 @@ ax[0].set_title('Total Reward vs Episodes')
 ax[0].legend()
 ax[0].grid(alpha=0.3)
 
-ax[1].plot([i for i in range(1, N_episodes+1)], episode_number_of_steps, label='Steps per episode')
+ax[1].plot([i for i in range(1, N_episodes+1)],
+           episode_number_of_steps, label='Steps per episode')
 ax[1].plot([i for i in range(1, N_episodes+1)], running_average(
     episode_number_of_steps, n_ep_running_average), label='Avg. number of steps per episode')
 ax[1].set_xlabel('Episodes')
